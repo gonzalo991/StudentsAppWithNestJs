2dadd1736b6bed247be8f3d1ee23c9f0c11f006c
Gonzalo Araya <email@mail.com>
2024-07-22 22:14:48 -0300

    configuracion de modulo pupils

--- src/pupils/pupils.module.ts
+++ src/pupils/pupils.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { PupilsService } from './pupils.service';
 import { PupilsController } from './pupils.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Pupil, PupilSchema } from './schemas/pupils.schema';
+import { Subject, SubjectSchema } from './schemas/subject.schema';
+import { PupilFactoryImpl } from './schemas/factories/pupil.factory';
+import { SubjectFactoryImpl } from './schemas/factories/subject.factory';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Pupil.name, schema: PupilSchema }]),
+    MongooseModule.forFeature([{ name: Subject.name, schema: SubjectSchema }]),
+  ],
   controllers: [PupilsController],
-  providers: [PupilsService],
+  providers: [PupilsService,
+    { provide: 'PupilFactory', useClass: PupilFactoryImpl },
+    { provide: 'SubjectFactory', useClass: SubjectFactoryImpl},
+  ],
 })
-export class PupilsModule {}
+
+export class PupilsModule { }
--- src/pupils/pupils.service.ts
+++ src/pupils/pupils.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePupilDto } from './dto/create-pupil.dto';
 import { UpdatePupilDto } from './dto/update-pupil.dto';
+import { PupilFactory } from './schemas/factories/pupil_factory.interface';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class PupilsService {
+
+  constructor(
+    @Inject('PupilFactory') private readonly pupilFactorty: PupilFactory,
+  ){}
+
   create(createPupilDto: CreatePupilDto) {
     return 'This action adds a new pupil';
   }
