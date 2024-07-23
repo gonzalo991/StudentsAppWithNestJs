643698c810b0a605988ed1958fa2c07faacc99c3
Gonzalo Araya <email@mail.com>
2024-07-22 22:33:10 -0300

    crud en pupil service

--- src/pupils/pupils.service.ts
+++ src/pupils/pupils.service.ts
@@ -1,33 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreatePupilDto } from './dto/create-pupil.dto';
 import { UpdatePupilDto } from './dto/update-pupil.dto';
 import { PupilFactory } from './schemas/factories/pupil_factory.interface';
 import { InjectModel } from '@nestjs/mongoose';
+import { Pupil } from './schemas/pupils.schema';
+import { SubjectFactory } from './schemas/factories/subject_factory.interface';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class PupilsService {
 
   constructor(
-    @Inject('PupilFactory') private readonly pupilFactorty: PupilFactory,
-  ){}
+    @Inject('PupilFactory') private readonly pupilFactory: PupilFactory,
+    @Inject('SubjectFactory') private readonly subjectFactory: SubjectFactory,
+    @InjectModel(Pupil.name) private readonly pupilModel: Model<Pupil>,
+  ) { }
 
-  create(createPupilDto: CreatePupilDto) {
-    return 'This action adds a new pupil';
+  async create(createPupilDto: CreatePupilDto): Promise<Pupil> {
+    const { name, surname, dni, section, subjects } = createPupilDto;
+
+    const subjectInstance = subjects.map(subject => this.subjectFactory
+      .createSubject(subject.subjectName, subject.qualification));
+
+    const pupil = this.pupilFactory.createPupil(name, surname, dni, section, subjectInstance);
+    const createdPupil = new this.pupilModel(pupil);
+
+    return await createdPupil.save();
   }
 
-  findAll() {
-    return `This action returns all pupils`;
+  async findAll(): Promise<Pupil[]> {
+    return await this.pupilModel.find().exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pupil`;
+  async findOne(id: string): Promise<Pupil> {
+    return await this.pupilModel.findById(id).exec();
   }
 
-  update(id: number, updatePupilDto: UpdatePupilDto) {
-    return `This action updates a #${id} pupil`;
+  async update(id: string, updatePupilDto: UpdatePupilDto) {
+    const existingPupil = await this.pupilModel.findById(id).exec();
+
+    if (!existingPupil) {
+      throw new Error(`Pupil with id: ${id} does not exists`);
+    }
+
+    const { name, surname, dni, section, subjects } = updatePupilDto;
+
+    if (name !== undefined) existingPupil.name = name;
+    if (surname !== undefined) existingPupil.surname = surname;
+    if (dni !== undefined) existingPupil.dni = dni;
+    if (section !== undefined) existingPupil.section = section;
+
+    if (subjects !== undefined) {
+      const subjectInstances = subjects.map(subject => this.subjectFactory
+        .createSubject(subject.subjectName, subject.qualification));
+      existingPupil.subjects = subjectInstances;
+    }
+
+    return await existingPupil.save();
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pupil`;
+  async remove(id: string): Promise<Pupil> {
+    return await this.pupilModel.findOneAndDelete(id).exec();
   }
 }
