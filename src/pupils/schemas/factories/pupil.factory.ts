1bcea527ad081c5eafd50999910b0184a73c2891
Gonzalo Araya <email@mail.com>
2024-07-27 20:59:15 -0300

    documentando factory method

--- src/pupils/schemas/factories/pupil.factory.ts
+++ src/pupils/schemas/factories/pupil.factory.ts
@@ -2,6 +2,12 @@ import { Subject } from "src/subjects/schemas/subject.schema";
 import { Pupil } from "../pupils.schema";
 import { PupilFactory } from "./pupil_factory.interface";
 
+/**
+ * Implementación del patrón de diseño factory method
+ * Define un metodo que recibe los parámetros necesarios para crear 
+ * una instancia de alumno mediante dichos parámetros
+ * Devueolve la instancia de alumno
+ */
 export class PupilFactoryImpl implements PupilFactory {
     createPupil(name: string, surname: string, dni: string, section: string, subjects: Subject[]): Pupil {
         const pupil = new Pupil();
--- src/pupils/schemas/factories/pupil_factory.interface.ts
+++ src/pupils/schemas/factories/pupil_factory.interface.ts
@@ -1,5 +1,8 @@
 import { Pupil } from "../pupils.schema";
 
+/**
+ * Se crea una interface para la implementación del patrón de diseño factory method
+ */
 export interface PupilFactory {
     createPupil(name:string, surname:string, dni:string, section:string, subjects:any[]):Pupil;
 }
