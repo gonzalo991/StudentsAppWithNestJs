538acef3b2890f4435214539cead1477b34285ff
Gonzalo Araya <email@mail.com>
2024-07-27 20:52:39 -0300

    documentando los services

--- src/pupils/pupils.service.ts
+++ src/pupils/pupils.service.ts
@@ -7,66 +7,162 @@ import { Pupil } from './schemas/pupils.schema';
 import { Model } from 'mongoose';
 import { Subject } from 'src/subjects/schemas/subject.schema';
 
+/**
+ * Servicio para manejar la lógica de negocio relacionada con los alumnos
+ */
 @Injectable()
 export class PupilsService {
 
+  /**
+   * Constructor que crea una nueva instancuia de PupilsService
+   * @param pupilFactory fabrica utilizada para crear instancias de alumnos
+   * @param subjectModel modelo de las asignaturas
+   * @param pupilModel  modelo de los alumnos
+   */
   constructor(
     @Inject('PupilFactory') private readonly pupilFactory: PupilFactory,
     @InjectModel(Subject.name) private readonly subjectModel: Model<Subject>,
     @InjectModel(Pupil.name) private readonly pupilModel: Model<Pupil>,
   ) { }
 
+  /**
+   * Método para crear un alumno en la base de datos
+   * Utiliza los datos del DTO
+   * Evalúa si el dni y las asignaturas(subjects) existen entre los datos enviados por el usuario
+   * En caso de existir crea una constante en donde se guardarán los valores de las asignaturas
+   * que serán extraidos de la base de datos usando el dni del alumno para relacionarlos
+   * Usa la fabrica de alumnos para crear una instancia de alumno
+   * Crea una nueva instancia del model con la instancia ya creada por la fábrica
+   * Guarda la instancia en la base de datos
+   * @param createPupilDto Datos necesarios para instanciar el alumno
+   * @returns devuelve el alumno recién creado
+   * @throws Devuelve un mensaje de error y el tipo de error en caso de que haya algun problema
+   * durante la creación y el guardado del alumno
+   */
   async create(createPupilDto: CreatePupilDto): Promise<Pupil> {
     try {
+      // Obtiene los datos del DTO
       let { name, surname, dni, section, subjects } = createPupilDto;
 
+      // Evalua si el usuario envió los valores dni y subjects para luego crear
+      // una constante con los valores de las asignatruras encontradas en la base de datos
       if (dni && subjects.length === 0) {
         const subjectDocuments = await this.subjectModel.find({ pupilDni: dni }).exec();
-        subjectDocuments ? subjects.push(subjectDocuments
-          .map(doc => doc.toObject())) : console.info(`El documento ${subjectDocuments} está vacío`);
+        subjectDocuments ? subjects = subjectDocuments
+          .map(doc => doc.toObject()) : console.info(`El documento ${subjectDocuments} está vacío`);
       }
 
+      // Crea una instancia de alumno usando la fábrica
       const pupil = this.pupilFactory.createPupil(name, surname, dni, section, subjects);
+      // Crea una nueva instancia del modelo de alumno con la instancia anterior
       const createdPupil = new this.pupilModel(pupil);
 
+      // Muestra un mensaje de éxito y guarda el alumno en la base de datos
+      console.info(`El alumno se creó correctamente con los datos: ${createdPupil}`);
       return await createdPupil.save();
 
     } catch (error) {
-
+      // Muestra un mensaje con el error en caso de que esto ocurra
       throw new Error(`Ocurrio un error al crear el alumno: \n
         ${error}`);
-
     }
   }
 
+  /**
+   * Busca en la base de datos todos los alumnos existentes
+   * @returns Devuelve una lista de todos los alumnos en formato JSON
+   */
   async findAll(): Promise<Pupil[]> {
     return await this.pupilModel.find().exec();
   }
 
-  async findOne(id: string): Promise<Pupil> {
-    return await this.pupilModel.findById(id).exec();
+  /**
+   * Busca en la base de datos el alumno que coincida con el parametro enviado
+   * por el usuario
+   * @param dni parametro de busqueda proporcionado por el usuario
+   * @returns Devuelve el registro del alumno que coincida con el parametro de busqueda
+   */
+  async findOne(dni: string): Promise<Pupil> {
+    // Crea una constante en la cual se guardará el registro buscado en la base de datos
+    const existingPupil = await this.pupilModel.findOne({dni}).exec();
+
+    // Evalúa si se encontró el registro buscado
+    if(existingPupil){
+      // En caso de encontrar el registro devuelve un mensaje de exito y el registro
+      console.info(`Alumno con dni: ${dni} encontrado exitosamente.`);
+      return existingPupil;
+    } else {
+      // Caso contrario devuelve un mensaje informando que la busqueda no tuvo exito
+      console.info(`No se encontró el alumno con dni: ${dni} en la base de datos.`);
+    }
   }
 
-  async update(id: string, updatePupilDto: UpdatePupilDto) {
-    
-    const existingPupil = await this.pupilModel.findById(id).exec();
+  /**
+   * Actualiza los valores del alumno
+   * Busca en la base de datos el registro del alumno que coincida con el parametro proporcionado
+   * Evalúa si se encontró dicho registro y en caso de no encontrarlo lanza un error
+   * Caso contrario se guarda el id del registro en una constante
+   * Se usa los datos del DTO para actualizar el alumno
+   * Se evalua si existen los valores en el registro y se actualizan en caso de existir
+   * Se devuelve el alumno actualizado
+   * @param dniToFind Parametro proporcionado por el usuario para la busqueda
+   * @param updatePupilDto Datos necesario para actualizar el alumno
+   * @returns Devuelve el alumno actualizado
+   */
+  async update(dniToFind: string, updatePupilDto: UpdatePupilDto) {
 
+    // Guarda el registro buscado en la base de datos
+    const existingPupil = await this.pupilModel.findOne({ dni: dniToFind }).exec();
+
+    // Evalua si existen valores en la constante
     if (!existingPupil) {
-      throw new Error(`Pupil with id: ${id} does not exists`);
+      // Lanza un error si no se cumple la condición
+      throw new Error(`Pupil with id: ${dniToFind} does not exists`);
     }
 
-    const { name, surname, dni, section, subjects } = updatePupilDto;
+    // Guarda el id del registro en una constante
+    const _id = existingPupil._id;
+    // Utiliza los datos del DTO para luego actualizar el alumno
+    const { name, surname, dni, section } = updatePupilDto;
 
+    // Se evalua si existen los valores en el registro y luego se los actualiza
     if (name !== undefined) existingPupil.name = name;
     if (surname !== undefined) existingPupil.surname = surname;
     if (dni !== undefined) existingPupil.dni = dni;
     if (section !== undefined) existingPupil.section = section;
-    existingPupil.subjects = subjects;
 
-    return await existingPupil.save();
+    // Devuelve el alumno con su registro actualizado
+    return await this.pupilModel.findByIdAndUpdate(_id, existingPupil);
   }
 
-  async remove(id: string): Promise<Pupil> {
-    return await this.pupilModel.findByIdAndDelete(id).exec();
+  /**
+   * Borra de la base de datos un alumno
+   * Busca el alumno que coincida con el parametro dni
+   * Evalúa si existe en la base de datos
+   * Guarda el id del alumno en una constante en caso de existir
+   * Borra el registro y manda un mensaje de éxito en la operación
+   * Caso contrario envía un mensaje informando que no se encontró el registro
+   * Devuelve los valores del alumno borrado
+   * @param dni Parametro de busqueda para borrar el alumno
+   * @returns Devuelve el registro que se acaba de borrar
+   */
+  async remove(dni: string): Promise<Pupil> {
+    // Guarda los valores encontrados en la base de datos en una constante
+    const pupilToDelete = await this.pupilModel.findOne({ dni }).exec();
+
+    // Evalúa si se encontró el registro
+    if (pupilToDelete) {
+      // Guarda el id en una constante
+      const _id = pupilToDelete._id;
+      // Borra el registro buscandolo con el id antes guardado
+      await this.pupilModel.findByIdAndDelete(_id).exec();
+      // Envía un mensaje de éxito
+      console.info(`El registro del alumno con dni: ${dni} se borró correctamente.`);
+    } else {
+      // Envía un mensaje de que no existe tal regístro
+      console.warn("No se encontró el registro a remover.")
+    }
+    // Devuelve el alumno borrado
+    return pupilToDelete;
   }
 }
--- src/subjects/subjects.service.ts
+++ src/subjects/subjects.service.ts
@@ -6,60 +6,114 @@ import { Subject } from './schemas/subject.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { SubjectFactory } from './schemas/factories/subject_factory.interface';
 
+/**
+ * Servicio para manejar la lógica de negocio relacionada con las asignaturas.
+ */
 @Injectable()
 export class SubjectsService {
 
+  /**
+   * Crea una nueva instancia de `SubjectsService`.
+   * @param subjectFactory - Fábrica utilizada para crear instancias de `Subject`.
+   * @param subjectModel - Modelo de Mongoose para `Subject`.
+   */
   constructor(
     @Inject('SubjectFactory') private readonly subjectFactory: SubjectFactory,
     @InjectModel(Subject.name) private readonly subjectModel: Model<Subject>
   ) { }
 
+  /**
+   * Crea una nueva asignatura en la base de datos.
+   * - Extrae los datos del DTO.
+   * - Utiliza la fábrica de asignaturas para crear una instancia de `Subject`.
+   * - Crea una nueva instancia del modelo `Subject` con la instancia creada.
+   * - Guarda la nueva asignatura en la base de datos.
+   * @param createSubjectDto - Datos necesarios para crear una nueva asignatura.
+   * @returns La asignatura recién creada.
+   * @throws Error si ocurre un problema al crear la asignatura.
+   */
   async create(createSubjectDto: CreateSubjectDto): Promise<Subject> {
     try {
-
+      // Extrae los datos del DTO
       const { pupilDni, subjectName, qualification } = createSubjectDto;
-      const subjectInstance = this.subjectFactory.createSubject(pupilDni, subjectName, qualification);
-      const createdSubject = new this.subjectModel(subjectInstance);
-      createdSubject ? console.info("Se creo correctamente lña asignatura.") : console.error("Algo pasó al asignar los datos.");
-      return await createdSubject.save();
 
+      // Usa la fábrica para crear una nueva instancia de asignatura
+      const subjectInstance = this.subjectFactory.createSubject(pupilDni, subjectName, qualification);
+
+      // Crea una nueva instancia del modelo con la instancia de asignatura
+      const createdSubject = new this.subjectModel(subjectInstance);
+
+      // Verifica si la asignatura fue creada exitosamente
+      createdSubject ? console.info("Se creó correctamente la asignatura.") : console.error("Algo pasó al asignar los datos.");
+
+      // Guarda la nueva asignatura en la base de datos
+      return await createdSubject.save();
     } catch (error) {
-      throw new Error(`Ocurrió un error al crear la asignatura: \n
-      ${error}`);
+      // Manejo de errores al crear la asignatura
+      throw new Error(`Ocurrió un error al crear la asignatura: \n${error}`);
     }
   }
 
-  async findAll() {
+  /**
+   * Obtiene todas las asignaturas de la base de datos.
+   * - Consulta todos los documentos en la colección de asignaturas.
+   * @returns Una lista de todas las asignaturas.
+   */
+  async findAll(): Promise<Subject[]> {
+    // Devuelve todas las asignaturas en la base de datos
     return await this.subjectModel.find().exec();
   }
 
-  async findOne(pupilDni: string) {
+  /**
+   * Busca una asignatura por el DNI del alumno.
+   * - Realiza una búsqueda en la colección de asignaturas utilizando el DNI del alumno.
+   * - Si se encuentra la asignatura, la devuelve; si no, devuelve `null`.
+   * @param pupilDni - DNI del alumno asociado a la asignatura.
+   * @returns La asignatura encontrada o `null` si no existe.
+   * @throws Error si ocurre un problema al buscar la asignatura.
+   */
+  async findOne(pupilDni: string): Promise<Subject | null> {
     try {
-
+      // Busca una asignatura con el DNI del alumno
       const subjectExist = await this.subjectModel.findOne({ pupilDni }).exec();
-      subjectExist ? console.info(`Se encontro el documento correspondiente al alumno con dni: ${pupilDni}`) : console.error(`Documento inexistente`);
+
+      // Verifica si se encontró el documento
+      subjectExist ? console.info(`Se encontró el documento correspondiente al alumno con DNI: ${pupilDni}`) : console.error(`Documento inexistente`);
+
+      // Devuelve la asignatura encontrada o `null`
       return subjectExist;
-
     } catch (error) {
-
-      throw new Error(`Ocurrió un error al buscar el documento solicitado: \n
-        ${error}`);
-
+      // Manejo de errores al buscar la asignatura
+      throw new Error(`Ocurrió un error al buscar el documento solicitado: \n${error}`);
     }
   }
 
+  /**
+   * Actualiza una asignatura existente.
+   * - Busca la asignatura por ID.
+   * - Si la asignatura existe, actualiza los datos con los del DTO.
+   * - Guarda los cambios en la base de datos.
+   * @param id - ID de la asignatura a actualizar.
+   * @param updateSubjectDto - Datos con los que se actualizará la asignatura.
+   * @returns La asignatura actualizada.
+   * @throws Error si ocurre un problema al actualizar la asignatura o si la asignatura no existe.
+   */
   async update(id: string, updateSubjectDto: UpdateSubjectDto): Promise<Subject> {
     try {
+      // Busca la asignatura por ID
+      const existingSubject = await this.subjectModel.findById(id).exec();
 
-      const existingSubject = await this.subjectModel.findByIdAndUpdate(id).exec();
+      if (!existingSubject) {
+        // Lanza un error si la asignatura no existe
+        throw new Error(`La asignatura con ID: ${id} no existe`);
+      } else {
+        console.info("Se encontró el documento, procediendo con los cambios...");
+      }
 
-      if (existingSubject)
-        throw new Error(`The subjects with pupil dni: ${id} doesn't exists`);
-      else
-        console.info("Se encontró el documento, procediendo con los cambios...")
-
+      // Extrae los datos del DTO
       const { pupilDni, subjectName, qualification } = updateSubjectDto;
 
+      // Actualiza los datos de la asignatura
       if (existingSubject.pupilDni !== undefined && existingSubject.subjectName !== undefined
         && existingSubject.qualification !== undefined) {
         existingSubject.pupilDni = pupilDni;
@@ -67,29 +121,37 @@ export class SubjectsService {
         existingSubject.qualification = qualification;
         console.info("Cambios realizados correctamente");
 
+        // Guarda los cambios en la base de datos
         return await existingSubject.save();
       } else {
         throw new Error("No se realizaron los cambios solicitados.");
       }
-
     } catch (error) {
-
-      throw new Error(`Ocurrió un error al actualizar el documento: \n
-        ${error}`)
-
+      // Manejo de errores al actualizar la asignatura
+      throw new Error(`Ocurrió un error al actualizar el documento: \n${error}`);
     }
   }
 
-  async remove(id: string) {
+  /**
+   * Elimina una asignatura por ID.
+   * - Busca la asignatura por ID.
+   * - Si la asignatura existe, la elimina de la base de datos.
+   * @param id - ID de la asignatura a eliminar.
+   * @throws Error si ocurre un problema al eliminar la asignatura o si la asignatura no existe.
+   */
+  async remove(id: string): Promise<void> {
     try {
+      // Busca la asignatura por ID
       const subjectExists = await this.subjectModel.findById(id).exec();
       if (subjectExists) {
+        // Elimina la asignatura si existe
         await this.subjectModel.findByIdAndDelete(id).exec();
         console.info("El documento se borró correctamente");
       } else {
         console.warn("No se encontró el documento solicitado para removerlo");
       }
     } catch (error) {
+      // Manejo de errores al eliminar la asignatura
       throw new Error("No se pudo borrar el documento");
     }
   }
